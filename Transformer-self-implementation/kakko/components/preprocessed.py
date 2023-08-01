c4f0aa1a75100359d5519e2f41438737be216864
neromehiro <neromehiro1014@gmail.com>
2023-08-02 02:26:42 +0900

    tokenizeがうまくいった

--- Transformer-self-implementation/kakko/components/preprocessed.py
+++ Transformer-self-implementation/kakko/components/preprocessed.py
@@ -3,9 +3,11 @@ import os
 import json
 from torch.utils.data import Dataset, DataLoader
 import glob
+import random
+from typing import List, Tuple
 
-# 括弧の種類
-tokens = ["(", ")", "[", "]", "{", "}"]
+# 括弧の種類とキーワード
+tokens = ["(", ")", "[", "]", "{", "}", "input", ",output" ,","]
 
 # トークンとIDを対応付ける辞書
 token2id = {token: i for i, token in enumerate(tokens)}
@@ -20,9 +22,43 @@ dirs = {
     "preprocessed": "/app/Transformer-self-implementation/kakko/kakko_dataset/preprocessed",
 }
 
+# 開括弧と閉括弧の対応関係
+brackets = {'(': ')', '[': ']', '{': '}'}
+
+BRACKETS = [('(', ')'), ('[', ']'), ('{', '}')]
+
 def ensure_dir(directory):
     if not os.path.exists(directory):
         os.makedirs(directory)
+
+def tokenize_string(string):
+    # ":"やダブルクォーテーションを削除
+    string = string.replace(":", "").replace("\"", "")
+    tokenized_string = []
+    start = 0
+    for i in range(len(string)):
+        if string[i] in tokens:
+            if start < i:
+                tokenized_string.append(string[start:i])
+            tokenized_string.append(string[i])
+            start = i+1
+
+    # 'input'を含むトークンが2回目以降に出現したときに、その前のトークンでリストを分割
+    result = []
+    temp = []
+    count = 0
+    for token in tokenized_string:
+        if "input" in token:
+            count += 1
+            if count > 1:
+                result.append(temp)
+                temp = []
+        temp.append(token)
+    result.append(temp)  # 最後のリストを追加
+
+    return result
+
+
 
 def preprocess_and_save_dataset(dataset, filepath):
     # 保存先ディレクトリの存在を確認し、存在しなければ作成
@@ -34,19 +70,82 @@ def preprocess_and_save_dataset(dataset, filepath):
         json.dump(dataset, f)
 
     # トークン化したデータセットを保存
-    tokenized_dataset = [[token for token in sequence] for sequence in dataset]
+    dataset_string = json.dumps(dataset)
+    tokenized_dataset = tokenize_string(dataset_string)
     with open(os.path.join(dirs["tokenize"], filepath), "w") as f:
         json.dump(tokenized_dataset, f)
 
     # 前処理したデータセットを保存
-    preprocessed_dataset = [[token2id[token] for token in sequence] for sequence in tokenized_dataset]
+    preprocessed_dataset = [[token2id[token] for token in token_list if token in token2id] for token_list in tokenized_dataset]
     with open(os.path.join(dirs["preprocessed"], filepath), "w") as f:
         json.dump(preprocessed_dataset, f)
 
-# データセットの作成と保存
-dataset_dir = "/app/Transformer-self-implementation/kakko/kakko_dataset/original"
-datasets = glob.glob(f"{dataset_dir}/*.json")
-for dataset_path in datasets:
-    with open(dataset_path, "r") as f:
-        dataset = json.load(f)
-    preprocess_and_save_dataset(dataset, os.path.basename(dataset_path))
+
+# 以下の関数を追加
+def generate_bracket_sequence(depth: int) -> str:
+    if depth == 0:
+        return ""
+    
+    num_brackets = random.randint(1, 3)
+    brackets = ""
+    for _ in range(num_brackets):
+        bracket = random.choice(BRACKETS)
+        inner = generate_bracket_sequence(depth - 1) if random.random() < 0.5 else ""
+        brackets += bracket[0] + inner + bracket[1]
+    
+    return brackets
+
+def split_sequence(seq: str) -> Tuple[str, str]:
+    if all(c in (')', ']', '}') for c in seq):
+        return seq[:-1], seq[-1:]
+    
+    for i in range(len(seq) - 1, -1, -1):
+        if seq[i] in ('(', '[', '{'):
+            return seq[:i+1], seq[i+1:]
+    
+    return seq, ""
+
+def generate_brackets(n_samples: int, max_depth: int, min_len: int, max_len: int) -> List[str]:
+    dataset = []
+    for _ in range(n_samples):
+        while True:
+            depth = random.randint(1, max_depth)
+            sequence = generate_bracket_sequence(depth)
+            if min_len <= len(sequence) <= max_len:
+                break
+        input_seq, output_seq = split_sequence(sequence)
+        # シーケンスを"input:~~,output:~~"の形式の文字列に変換
+        dataset.append(f"input:{input_seq},output:{output_seq}")
+    
+    return dataset
+
+
+# "generate_dataset"の呼び出しを"generate_brackets"の呼び出しに置き換え
+num_samples = 3  # データセットのサンプル数
+max_depth = 5  # 括弧の最大深さ
+min_len = 5  # シーケンスの最小長
+max_len = 10  # シーケンスの最大長
+# ファイル名：/app/Transformer-self-implementation/kakko/components/preprocessed.py
+
+def tokenize_dataset(dataset: List[str]) -> List[List[str]]:
+    """
+    与えられたデータセット（"input:~~,output:~~"の形式の文字列のリスト）をトークン化します。
+    具体的には、各文字列をカンマで分割し、それぞれの部分を個別にトークン化します。
+    """
+    tokenized_dataset = []
+    for string in dataset:
+        # 各文字列をトークン化
+        tokenized_string = tokenize_string(string)
+        # トークン化した文字列を追加
+        tokenized_dataset.append(tokenized_string)
+    return tokenized_dataset
+
+
+# データセットの生成
+dataset = generate_brackets(num_samples, max_depth, min_len, max_len)
+
+# データセットのトークン化
+tokenized_dataset = tokenize_dataset(dataset)
+
+# データセットの前処理と保存
+preprocess_and_save_dataset(dataset, "bracket_dataset.json")
--- Transformer-self-implementation/kakko/components/test_transformer.py
+++ Transformer-self-implementation/kakko/components/test_transformer.py
@@ -22,25 +22,34 @@ model_path = "/app/Transformer-self-implementation/kakko/models/mymodel0.h5"
 model = load_model(model_path)
 
 
-def predict_next_token(model, token_sequence):
+# 括弧の組み合わせが正しいかどうかを確認する関数
+def is_valid_bracket_sequence(seq):
+    bracket_pairs = {'(': ')', '[': ']', '{': '}'}
+    stack = []
+    for bracket in seq:
+        if bracket in bracket_pairs:  # 開始括弧
+            stack.append(bracket)
+        elif not stack or bracket_pairs[stack.pop()] != bracket:  # 閉じ括弧
+            return False
+    return not stack  
+
+def predict_next_token(model, token_sequence, k):
     # 入力のトークンをIDに変換
     input_sequence = [token2id[token] for token in token_sequence]
     # モデルへの入力は (1, sequence_length) の形状である必要がある
     input_sequence = np.array(input_sequence)[np.newaxis, :]
     # モデルを使って予測を行う（verbose=0に設定して進捗ログを非表示にする）
     predictions = model.predict(input_sequence, verbose=0)
+    # 予測されるIDがtokensリストの範囲内に収まるように修正
+    predictions = predictions[0, :len(tokens)]
     # 最も確率の高いトークンのIDを取得
-    predicted_token_id = np.argmax(predictions[0])
+    top_k_ids = np.argsort(-predictions)[:k]  # Top-kトークンIDを取得
+    predicted_token_id = np.random.choice(top_k_ids)  # Top-kの中からランダムに選択
     # IDをトークンに戻す
     predicted_token = id2token[predicted_token_id]
     return predicted_token
 
 
-# ファイル名：/app/Transformer-self-implementation/kakko/components/test_transformer.py
-
-# 以下のコードは変更せずにそのまま続けます。
-
-# ...
 
 def generate_bracket_string(max_depth, length, bracket_types):
     assert max_depth <= len(bracket_types), "Not enough bracket types for the given depth"
@@ -73,7 +82,7 @@ def generate_bracket_string(max_depth, length, bracket_types):
     
     return string
 
-def test_model(model, num_samples):
+def test_model(model, num_samples, k):
     bracket_types = [('(', ')'), ('[', ']'), ('{', '}')]
     correct_predictions = 0
     total_predictions = 0
@@ -92,12 +101,15 @@ def test_model(model, num_samples):
     for sample_id in range(num_samples):
         string = generate_bracket_string(3, 30, bracket_types)
         prediction_output = ""
+        correct_output = ""
         for i in range(len(string) - 1):
             token_sequence = list(string[:i+1])
             correct_next_token = string[i+1]
-            predicted_token = predict_next_token(model, token_sequence)
+            predicted_token = predict_next_token(model, token_sequence, k)  # トップ-k サンプリングを使用
             prediction_output += predicted_token
-            if predicted_token == correct_next_token:
+            correct_output += correct_next_token
+            # 予測が正しい括弧の組み合わせならば、正解としてカウント
+            if is_valid_bracket_sequence(prediction_output):
                 correct_predictions += 1
             total_predictions += 1
 
@@ -108,8 +120,9 @@ def test_model(model, num_samples):
         try:
             with open(file_path, "a") as f:  # "a" モードで追記
                 f.write(f"Test{sample_id + 1} Accuracy: {accuracy * 100}%\n")
-                f.write(f"Input : {string}\n")
-                f.write(f"Output: {prediction_output}\n\n")  # 追加: 各テストの結果を区切るための空行
+                f.write(f"Input  : {string}\n")
+                f.write(f"Output : {prediction_output}\n")
+                f.write(f"Correct: {correct_output}\n\n")  # 追加: 各テストの結果を区切るための空行
         except Exception as e:
             print(f"Failed to write to file {file_path}. Error: {e}")
 
@@ -118,4 +131,4 @@ def test_model(model, num_samples):
     print(f"Model accuracy: {overall_accuracy * 100}%")
 
 # test_model関数を呼び出す
-test_model(model, 10)  # ここでは、テストサンプル数を10としています。
+test_model(model, 10, 5)
